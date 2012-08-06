c7178a1c6670887d8eb8191017a776fd07bab2d0
Jason von Nieda <jason@vonnieda.org>
2012-08-05 21:30:08 -0700

    Grbl now emits an "ok" after startup, letting a listening program know it's okay to proceed.

--- protocol.c
+++ protocol.c
@@ -115,6 +115,7 @@ void protocol_init()
   // Print grbl initialization message
   printPgmString(PSTR("\r\nGrbl " GRBL_VERSION));
   printPgmString(PSTR("\r\n'$' to dump current settings\r\n"));
+  status_message(0);
 
   char_counter = 0; // Reset line input
   iscomment = false;
